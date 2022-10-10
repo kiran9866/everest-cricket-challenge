de283ba6ca01f50a44535421570f7965fedb2e5a
kiran <kiran22tulashaku7@gmail.com>
2022-10-11 00:00:29 +0530

    Parse give input file

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 import { sample } from 'lodash';
 import { outcomeByTiming } from './configuration';
+import { readFile } from './readFile';
 
 export function getOutcomeWhen(timing) {
 	return sample(outcomeByTiming[timing]);
 }
+
+export function parseInput(text) {
+	return text
+		.trim()
+		.split('\n')
+		.map((line) => line.split(' '))
+		.map(([bowlingCard, battingCard, shotTiming]) => ({
+			bowlingCard,
+			battingCard,
+			shotTiming,
+		}));
+}
+
+async function main() {
+	const text = await readFile();
+	const inputs = parseInput(text);
+	console.log(inputs);
+}
+
+main()
+	.then()
+	.catch((err) => {
+		console.error('Failed to run.');
+	});
